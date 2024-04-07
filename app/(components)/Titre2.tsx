dfafdd66d7df0b212fb8d9d68b78216bc6a35539
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-04-07 17:26:51 +0200

    3rd

--- app/(components)/Titre2.tsx
+++ app/(components)/Titre2.tsx
@@ -55,7 +55,7 @@ function Titre2() {
       </div>
       <div className="lg:w-[50%] flex gap-12 justify-center items-center">
         <Separator orientation="vertical" />
-        <iframe className="w-[200px] h-[112px] lg:w-[560px] lg:h-[315px] rounded-lg" src="https://www.youtube.com/embed/qY9gK2Gqwik?si=YoFbk5WgSaM0FfF1" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe className="w-[200px] h-[112px] lg:w-[560px] lg:h-[315px] rounded-lg" src="https://www.youtube.com/embed/qY9gK2Gqwik?si=YoFbk5WgSaM0FfF1" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
       </div>
     </div>
