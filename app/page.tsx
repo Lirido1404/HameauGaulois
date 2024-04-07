94bb1af221230d54467f6e0c6e1894ea166723e7
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-04-07 17:29:22 +0200

    4th

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
 
       <div className="flex flex-col gap-2 lg:flex-row mt-8 lg:mt-20 justify-center w-[70%] mx-auto">
         <div className="w-[50%] flex justify-end lg:border-b-4 lg:border-r-4 border-white p-1 rounded-lg ">
-        <iframe className="w-full h-full rounded-lg" src="https://www.youtube.com/embed/wHPNlgCMgVY?si=yTjdrzLR63CXYAlB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe className="w-full h-full rounded-lg" src="https://www.youtube.com/embed/wHPNlgCMgVY?si=yTjdrzLR63CXYAlB" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
         <div className="w-[50%] ">
         <p className="text-white text-justify p-2">
