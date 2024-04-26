8f9d7b2d62d675ac639e1fdbf4be65ba77ef959a
Lirido1404 <maxime.prevot1804kz@gmail.com>
2024-04-26 16:07:25 +0200

    ess2

--- app/(components)/CalandarComp.tsx
+++ app/(components)/CalandarComp.tsx
@@ -1,19 +1,31 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
 import { useDateStore } from "../(store)/store";
 
 function CalandarComp() {
   const [date, setDate] = useState<Date | undefined>(new Date());
   const vraidate = date?.toLocaleDateString();
-  const setDateInStore = useDateStore((state)=>state.setDate); // Fonction pour mettre à jour la date dans le store
-
+  const setDateInStore = useDateStore((state) => state.setDate); // Fonction pour mettre à jour la date dans le store
   // Utiliser useEffect pour mettre à jour la date dans le store lorsque la date change
+
+
+
   useEffect(() => {
     if (vraidate) {
       setDateInStore(vraidate);
     }
   }, [vraidate, setDateInStore]);
+
+  // Ajout des dates réservées pour les 3 et 4 mai
+  const bookedDays = [new Date(2024, 4, 3), new Date(2024, 4, 4)];
+
+  // Style pour les jours réservés
+  const bookedStyle = { border: "2px solid #1A73E8", borderRadius: "6px" };
+
+  // Modificateurs pour les jours réservés avec leur style
+  const modifiers = { booked: bookedDays };
+  const modifiersStyles = { booked: bookedStyle };
 
   return (
     <div>
@@ -22,6 +34,8 @@ function CalandarComp() {
         selected={date}
         onSelect={setDate}
         className="rounded-md border shadow bg-white text-black"
+        modifiers={modifiers} // Utilisation des modificateurs pour indiquer les jours réservés
+        modifiersStyles={modifiersStyles} // Utilisation des styles pour les modificateurs
       />
       <p className="text-center text-white">{vraidate}</p>
     </div>
